ff2d5984e0120150723e7c7c4ca94591d9ab3437
birte.kock <birte.kock@uxma.com>
2026-01-27 11:55:18 +0100

    Removed track selecion screen

--- screens/index.ts
+++ screens/index.ts
@@ -1,4 +1,3 @@
 export { HomeScreen } from './HomeScreen';
 export { InfoScreen } from './InfoScreen';
 export { LandingPageScreen } from './LandingPageScreen';
-export { TrackSelectionScreen } from './TrackSelectionScreen';
