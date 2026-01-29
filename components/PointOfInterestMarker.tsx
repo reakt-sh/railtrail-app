9fb220737d5c762d351e07fd2d61681f9497e0d3
birte.kock <birte.kock@uxma.com>
2026-01-29 14:24:02 +0100

    Improved POI Marker design

--- components/PointOfInterestMarker.tsx
+++ components/PointOfInterestMarker.tsx
@@ -1,6 +1,6 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import React, { memo } from 'react';
-import { PicnicIcon, TrackEndIcon, TurningPointIcon } from '../assets/icons';
+import { StyleSheet, View } from 'react-native';
 import { POIType } from '../types/init';
 import { Color } from '../values';
 
@@ -11,41 +11,61 @@ interface ExternalProps {
 
 type Props = ExternalProps;
 
+interface MarkerConfig {
+  icon: keyof typeof MaterialCommunityIcons.glyphMap | keyof typeof MaterialIcons.glyphMap;
+  color: string;
+  iconSizeSmall?: number;
+  iconSizeLarge?: number;
+
+  library?: 'MaterialIcons' | 'MaterialCommunityIcons';
+}
+
+const markerConfigs: Record<POIType, MarkerConfig> = {
+  [POIType.LevelCrossing]: {
+    icon: 'alpha-x',
+    color: Color.error,
+    iconSizeSmall: 20,
+    iconSizeLarge: 24,
+  },
+  [POIType.LesserLevelCrossing]: {
+    icon: 'warning-amber',
+    color: Color.warning,
+    library: 'MaterialIcons',
+  },
+  [POIType.Picnic]: { icon: 'silverware-fork-knife', color: Color.success },
+  [POIType.TrackEnd]: { icon: 'sign-direction', color: Color.track },
+  [POIType.TurningPoint]: { icon: 'rotate-left', color: Color.primary },
+  [POIType.Generic]: { icon: 'information-variant', color: Color.primary },
+};
+
 export const PointOfInterestMarker = memo(({ pointOfInterestType, useSmallMarker }: Props) => {
-  switch (pointOfInterestType) {
-    case POIType.LevelCrossing:
-      return (
-        <MaterialCommunityIcons
-          name="alpha-x-circle"
-          size={useSmallMarker ? 18 : 32}
-          color={Color.error}
-        />
-      );
-    case POIType.LesserLevelCrossing:
-      return (
-        <MaterialCommunityIcons
-          name="sign-caution"
-          size={useSmallMarker ? 18 : 32}
-          color={Color.warning}
-        />
-      );
-    case POIType.Picnic:
-      return <PicnicIcon width={useSmallMarker ? 16 : 28} height={useSmallMarker ? 16 : 28} />;
-    case POIType.TrackEnd:
-      return <TrackEndIcon width={useSmallMarker ? 16 : 28} height={useSmallMarker ? 16 : 28} />;
-    case POIType.TurningPoint:
-      return (
-        <TurningPointIcon width={useSmallMarker ? 16 : 28} height={useSmallMarker ? 16 : 28} />
-      );
-    case POIType.Generic:
-    // Generic markers
-    default:
-      return (
-        <MaterialCommunityIcons
-          name="information"
-          size={useSmallMarker ? 18 : 32}
+  const config = markerConfigs[pointOfInterestType] ?? markerConfigs[POIType.Generic];
+  const size = useSmallMarker ? 20 : 24;
+  const iconSize = useSmallMarker ? (config.iconSizeSmall ?? 12) : (config.iconSizeLarge ?? 16);
+
+  return (
+    <View style={[styles.circle, { width: size, height: size, backgroundColor: config.color }]}>
+      {config.library === 'MaterialIcons' ? (
+        <MaterialIcons
+          name={config.icon as keyof typeof MaterialIcons.glyphMap}
+          size={iconSize}
           color={Color.white}
         />
-      );
-  }
+      ) : (
+        <MaterialCommunityIcons
+          name={config.icon as keyof typeof MaterialCommunityIcons.glyphMap}
+          size={iconSize}
+          color={Color.white}
+        />
+      )}
+    </View>
+  );
+});
+
+const styles = StyleSheet.create({
+  circle: {
+    borderRadius: 999,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
