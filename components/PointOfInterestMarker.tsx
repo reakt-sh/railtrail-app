41608cfdec062ebdf26fd95d56ebaf53feba817d
birte.kock <birte.kock@uxma.com>
2026-01-29 14:01:59 +0100

    updated marker icons

--- components/PointOfInterestMarker.tsx
+++ components/PointOfInterestMarker.tsx
@@ -1,13 +1,8 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { memo } from 'react';
+import { PicnicIcon, TrackEndIcon, TurningPointIcon } from '../assets/icons';
 import { POIType } from '../types/init';
-import {
-  GenericMarkerIcon,
-  LevelCrossingIcon,
-  LesserLevelCrossingIcon,
-  PicnicIcon,
-  TrackEndIcon,
-  TurningPointIcon,
-} from '../assets/icons';
-import { memo } from 'react';
+import { Color } from '../values';
 
 interface ExternalProps {
   readonly pointOfInterestType: POIType;
@@ -19,19 +14,38 @@ type Props = ExternalProps;
 export const PointOfInterestMarker = memo(({ pointOfInterestType, useSmallMarker }: Props) => {
   switch (pointOfInterestType) {
     case POIType.LevelCrossing:
-      return <LevelCrossingIcon width={useSmallMarker ? 18 : 32} height={useSmallMarker ? 18 : 32} />;
+      return (
+        <MaterialCommunityIcons
+          name="alpha-x-circle"
+          size={useSmallMarker ? 18 : 32}
+          color={Color.error}
+        />
+      );
     case POIType.LesserLevelCrossing:
       return (
-        <LesserLevelCrossingIcon width={useSmallMarker ? 16 : 24} height={useSmallMarker ? 14 : 20} />
+        <MaterialCommunityIcons
+          name="sign-caution"
+          size={useSmallMarker ? 18 : 32}
+          color={Color.warning}
+        />
       );
     case POIType.Picnic:
       return <PicnicIcon width={useSmallMarker ? 16 : 28} height={useSmallMarker ? 16 : 28} />;
     case POIType.TrackEnd:
       return <TrackEndIcon width={useSmallMarker ? 16 : 28} height={useSmallMarker ? 16 : 28} />;
     case POIType.TurningPoint:
-      return <TurningPointIcon width={useSmallMarker ? 16 : 28} height={useSmallMarker ? 16 : 28} />;
+      return (
+        <TurningPointIcon width={useSmallMarker ? 16 : 28} height={useSmallMarker ? 16 : 28} />
+      );
     case POIType.Generic:
+    // Generic markers
     default:
-      return <GenericMarkerIcon width={useSmallMarker ? 12 : 20} height={useSmallMarker ? 12 : 20} />;
+      return (
+        <MaterialCommunityIcons
+          name="information"
+          size={useSmallMarker ? 18 : 32}
+          color={Color.white}
+        />
+      );
   }
 });
