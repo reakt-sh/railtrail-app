c41807778dc9ce545b8e32bc31e007ace25fc52d
birte.kock <birte.kock@uxma.com>
2026-03-02 13:13:45 +0100

    fixed icon position on android

--- components/POIMarker.tsx
+++ components/POIMarker.tsx
@@ -1,9 +1,6 @@
 import * as MapLibreGL from '@maplibre/maplibre-react-native';
-import React, { memo, useRef } from 'react';
-import { useTranslation } from '../hooks/useTranslation';
+import React, { memo } from 'react';
 import { PointOfInterest } from '../types/init';
-import { getPOITitle } from '../util/poi';
-import { POITooltip } from './POITooltip';
 import { PointOfInterestMarker } from './PointOfInterestMarker';
 
 interface Props {
@@ -15,28 +12,18 @@ interface Props {
 }
 
 /**
- * Displays a Point of Interest on the map with:
- * - An icon indicating the POI type (crossing, picnic area, etc.)
- * - A tooltip/callout showing details when tapped
+ * Displays a Point of Interest on the map with an icon indicating the POI type.
+ * Uses MarkerView instead of PointAnnotation to avoid Android bitmap snapshot
+ * rendering issues (icon centering broken due to view flattening).
  */
 export const POIMarker = memo(({ poi, index, zoomLevel }: Props) => {
-  const i18n = useTranslation();
-  const title = getPOITitle(i18n, poi.name, poi.typeId, poi.originalType);
-  const annotationRef = useRef<MapLibreGL.PointAnnotationRef>(null);
-
   return (
-    <MapLibreGL.PointAnnotation
-      ref={annotationRef}
+    <MapLibreGL.MarkerView
       key={`poi-${index}`}
       id={`poi-${index}`}
       coordinate={[poi.pos.lng, poi.pos.lat]}
-      title={title}
     >
       <PointOfInterestMarker pointOfInterestType={poi.typeId} zoomLevel={zoomLevel} />
-
-      <MapLibreGL.Callout title={title}>
-        <POITooltip name={poi.name} type={poi.typeId} originalType={poi.originalType} />
-      </MapLibreGL.Callout>
-    </MapLibreGL.PointAnnotation>
+    </MapLibreGL.MarkerView>
   );
 });
